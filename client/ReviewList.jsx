0602c93503a567ea6d2ee4784e94df9926264c22
jasminelopez <lopez.jasmine094@gmail.com>
2019-05-17 19:15:10 -0700

    complete setup for package.json -  create react components -  create seeder file -  create html -  create server file

---
+++ client/ReviewList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ReviewEntry from './ReviewEntry';
+
+var ReviewList = ({reviews}) => { 
+  return (
+    <div className="reviewList"> 
+      {
+        reviews.map(review => {
+        return <ReviewEntry review={ review } key={ review.id }/>
+      }) }
+      {console.log(reviews)}
+    </div>
+  );
+};
+
+// ReviewList.PropTypes = {
+//   reviews: reviews.PropTypes.array.isRequired
+// }
+
+export default ReviewList;
---
+++ database/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true })
+        .then(() => console.log('MongoDB connected...'))
+        .catch(err => console.log('error'));
+
+let reviewSchema = mongoose.Schema({
+  id: {type: Number, required: true, unique: true},
+  imagePath: {type: String, required: true},
+  name: {type: String, required: true},
+  postDate: {type: Date, required: true},
+  review: {type: String, required: true},
+});
+
+let Review = mongoose.model('Review', reviewSchema);
+
+module.exports.Review = Review;
