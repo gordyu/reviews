a60314d0b02f172d79ab3f2c74880f35c9ccb2f3
jasminelopez <lopez.jasmine094@gmail.com>
2019-05-27 23:26:12 -0700

    Implemented search component that filters through reviews and displays review with input text -  add handleSearch that searches with onKeyDown event

--- client/ReviewEntry.jsx
+++ client/ReviewEntry.jsx
@@ -8,29 +8,68 @@ import styled from 'styled-components';
 var ReviewEntry = ({review}) => {
     return (
       <Wrapper>
-        <img className="profileImage" src={faker.image.avatar()}  height="42" width="42" />
-        <UserName> { review.name } </UserName>
-        <PostDate> { faker.date.month() } { review.postDate } </PostDate>
-        <Review> { review.review } </Review>
+        <TopContainer>
+          <ImageContainer>
+            <Image className="profileImage" src={faker.image.avatar()}  height="42" width="42" />
+          </ImageContainer>
+          <NameAndDateContainer>
+            <UserNameContainer> { review.name } </UserNameContainer>
+            <PostDate> 
+            { faker.date.month() } { review.postDate } 
+            </PostDate>
+          </NameAndDateContainer>
+        </TopContainer>
+        <Review> 
+          { review.review } 
+        </Review>
       </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-  border-bottom: 1px;
+  border-bottom: solid;
   border-bottom-color: #E8E8E8;
+  border-bottom-width: 0.5px;
+  display: flex;
+  flex-direction: column;
+  padding: 22px 0;
 `;
 
-const Review = styled.h4`
+const TopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: space-around;
+  padding: 10px 0;
+`;
+
+const Image = styled.img` 
+  border-radius: 50%;
+`;
+
+const ImageContainer = styled.div`
+  margin-right: 15px;
+`;
+
+const NameAndDateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Review = styled.div`
   font-family: 'Nunito Sans', sans-serif;
 `;
 
-const UserName = styled.h4`
+const UserNameContainer = styled.div`
   font-family: 'Nunito Sans', sans-serif;
   font-weight: bold;
 `;
 
-const PostDate = styled.h5`
+// const UserName = styled.h4`
+//   font-family: 'Nunito Sans', sans-serif;
+//   font-weight: bold;
+// `;
+
+const PostDate = styled.div`
   font-family: 'Nunito Sans', sans-serif;
 `;
 
