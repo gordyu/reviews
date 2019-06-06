a7fedfcfbf10d0f7a338a6b033d1c8b80406c01f
isenese <ignaciosenese@gmail.com>
2019-06-06 09:39:05 +0930

    server and database advanced, not tested yet

--- database/index.js
+++ database/index.js
@@ -1,45 +1,132 @@
 //This is postgres
 
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true })
-        .then(() => console.log('MongoDB connected...'))
-        .catch(err => console.log('error'));
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'ignacio',
+  host: 'localhost',
+  database: 'reviews',
+  password: 'password',
+  port: 5432,
+})
+const getReviews = (request, response) => {
+  pool.query('SELECT * FROM reviews ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
-let reviewSchema = mongoose.Schema({
-  id: {type: Number, required: true, unique: true},
-  imagePath: {type: String, required: true},
-  name: {type: String, required: true},
-  postDate: {type: Number, required: true},
-  review: {type: String, required: true},
-  accuracyRating: {type: Number, required: true},
-  communicationRating: {type: Number, required: true},
-  cleanlinessRating: {type: Number, required: true},
-  locationRating: {type: Number, required: true},
-  checkinRating: {type: Number, required: true},
-  valueRating: {type: Number, required: true}
-});
+const getReviewsById = (request, response) => {
+  const id = parseInt(request.params.id)
 
-let Review = mongoose.model('Review', reviewSchema);
+  pool.query('SELECT * FROM reviews WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createReview = (request, response) => {
+  const { imagePath, name, postDate, review, accuracyRating, communicationRating, cleanlinessRating, locationRating, checkinRating, valueRating } = request.body
+
+  pool.query('INSERT INTO reviews (imagePath, name, postDate, review, accuracyRating, communicationRating, cleanlinessRating, locationRating, checkinRating, valueRating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [imagePath, name, postDate, review, accuracyRating, communicationRating, cleanlinessRating, locationRating, checkinRating, valueRating], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+}
+
+const updateReview = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { imagePath, name, postDate, review, accuracyRating, communicationRating, cleanlinessRating, locationRating, checkinRating, valueRating } = request.body
+
+  pool.query(
+    'UPDATE reviews SET imagePath = $1, name = $2, postDate = $3, review = $4, accuracyRating = $5, communicationRating = $6, cleanlinessRating = $7, locationRating = $8, checkinRating = $9, valueRating = $10  WHERE id = $3',
+    [imagePath, name, postDate, review, accuracyRating, communicationRating, cleanlinessRating, locationRating, checkinRating, valueRating, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Review modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteReview = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM reviews WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Review deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getReviews,
+  getReviewsById,
+  createReview,
+  updateReview,
+  deleteReview,
+}
 
 
-// let save = (review) => {
-//   //review =JSON.parse(review);
-//   console.log(typeof review);
-//   let newReview = new Review({
-//     imagePath: review.imagePath,
-//     name:review.name,
-//     postDate: review.postDate,
-//     review: review.review,
-//     accuracyRating: review.accuracyRating,
-//     communicationRating: reciew.communicationRating,
-//     cleanlinessRating: review.cleanlinessRating,
-//     locationRating: review.locationRating,
-//     checkinRating: review.checkinRating,
-//     valueRating: review.valueRating,
-//   })
-// }
 
 
-// module.exports.save = save;
-module.exports.Review = Review;
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// mongoose.set('useCreateIndex', true);
+// mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true })
+//         .then(() => console.log('MongoDB connected...'))
+//         .catch(err => console.log('error'));
+
+// let reviewSchema = mongoose.Schema({
+//   id: {type: Number, required: true, unique: true},
+//   imagePath: {type: String, required: true},
+//   name: {type: String, required: true},
+//   postDate: {type: Number, required: true},
+//   review: {type: String, required: true},
+//   accuracyRating: {type: Number, required: true},
+//   communicationRating: {type: Number, required: true},
+//   cleanlinessRating: {type: Number, required: true},
+//   locationRating: {type: Number, required: true},
+//   checkinRating: {type: Number, required: true},
+//   valueRating: {type: Number, required: true}
+// });
+
+// let Review = mongoose.model('Review', reviewSchema);
+
+
+// // let save = (review) => {
+// //   //review =JSON.parse(review);
+// //   console.log(typeof review);
+// //   let newReview = new Review({
+// //     imagePath: review.imagePath,
+// //     name:review.name,
+// //     postDate: review.postDate,
+// //     review: review.review,
+// //     accuracyRating: review.accuracyRating,
+// //     communicationRating: reciew.communicationRating,
+// //     cleanlinessRating: review.cleanlinessRating,
+// //     locationRating: review.locationRating,
+// //     checkinRating: review.checkinRating,
+// //     valueRating: review.valueRating,
+// //   })
+// // }
+
+
+// // module.exports.save = save;
+// module.exports.Review = Review;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 let app = express();
-const {Review} = require('../database/index');
+const db = require('../database/index');
 var cors = require('cors')
 
 
@@ -11,74 +11,81 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/../public'));
 app.use(cors());
 
-//get request to pull all of the review data onto the page
-app.get('/reviews', function(req, res) {
-  Review.find({}, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  })
-  .catch(err => {
-    console.error("error ", err);
-    res.status(400).json({err});
-  });
-});
+
+app.get('/reviews', db.getUsers)
+app.get('/reviews/:id', db.getUserById)
+app.post('/reviews', db.createUser)
+app.put('/reviews/:id', db.updateUser)
+app.delete('/reviews/:id', db.deleteUser)
+
+// //get request to pull all of the review data onto the page
+// app.get('/reviews', function(req, res) {
+//   Review.find({}, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.json(result);
+//     }
+//   })
+//   .catch(err => {
+//     console.error("error ", err);
+//     res.status(400).json({err});
+//   });
+// });
 
 
-// Read one review
-app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id, (err, result) => {
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      res.sendStatus(404);
-    }
-  });
-});
+// // Read one review
+// app.get('/reviews/:id', (req, res) => {
+//   Review.findById(req.params.id, (err, result) => {
+//     if (result) {
+//       res.status(200).json(result);
+//     } else {
+//       res.sendStatus(404);
+//     }
+//   });
+// });
 
-// Delete one review
-app.delete('/reviews/:id', (req, res) => {
+// // Delete one review
+// app.delete('/reviews/:id', (req, res) => {
 
-  Review.deleteOne(req.params._id, (err, result) => {
-    if (err) {
-      res.sendStatus(404);
-    } else {
-      res.sendStatus(204);
-      console.log('Review Deleted')
-    }
-  });
-});
+//   Review.deleteOne(req.params._id, (err, result) => {
+//     if (err) {
+//       res.sendStatus(404);
+//     } else {
+//       res.sendStatus(204);
+//       console.log('Review Deleted')
+//     }
+//   });
+// });
 
-//Update one review    TODO
-app.put('/reviews/:id', (req, res) => {
-  console.log(req.params)
-  Review.findByIdAndUpdate({_id:req.params.id}, req.body, (err, review) => {
-    if (review) {
-      res.status(200).json(review);
-    } else {
+// //Update one review    TODO
+// app.put('/reviews/:id', (req, res) => {
+//   console.log(req.params)
+//   Review.findByIdAndUpdate({_id:req.params.id}, req.body, (err, review) => {
+//     if (review) {
+//       res.status(200).json(review);
+//     } else {
 
-      res.sendStatus(404);
-    }
-  });
-});
+//       res.sendStatus(404);
+//     }
+//   });
+// });
 
-//Create Review
-app.post('/reviews', (req, res) => {
+// //Create Review
+// app.post('/reviews', (req, res) => {
 
-  // let review = new Review(req.body)
-  // Review.save()
-  //const review = new db.Review;
-  Review.create(req.body, (err, data) => {
-    if (err) {
-      console.log(err)
-      res.sendStatus(400);
-    } else {
-      res.status(201).json(data);
-    }
-  });
-});
+//   // let review = new Review(req.body)
+//   // Review.save()
+//   //const review = new db.Review;
+//   Review.create(req.body, (err, data) => {
+//     if (err) {
+//       console.log(err)
+//       res.sendStatus(400);
+//     } else {
+//       res.status(201).json(data);
+//     }
+//   });
+// });
 
 let port = 3003;
 
